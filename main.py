e5cf4840c711cdfa2d92ab06a02e2cf67eccb306
alexsmith922 <10asmith@gmail.com>
2021-09-26 14:15:35 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO 1: Prompt user by asking "What would you like? (espresso/latte/cappucino)"
+# drink_choice = input("What would you like? (espresso/latte/cappucino)")
+off = False
+# TODO 2: Turn off the Coffee Machine by entering "off" to the prompt
+# TODO 3: Print report that generate and shows current resources
+
+# TODO 4: Check that available resources are sufficient?
+# TODO 5: Process coins if resouces are sufficient
+# TODO 6: Check if transaction was successful
+# TODO 7: Make coffee
