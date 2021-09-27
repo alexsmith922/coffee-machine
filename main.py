97c5d29ee8574c2929e4116609eee2c58268da2c
alexsmith922 <10asmith@gmail.com>
2021-09-27 12:21:05 -0500

    Started over.

--- main.py
+++ main.py
@@ -24,29 +24,10 @@ MENU = {
     }
 }
 
+drink_choice = input("What would you like? (espresso/latte/cappuccino): ")
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
-def resource_report():
-    for resource, amount in resources.items():
-        print(f"There is {amount} of {resource} remaining")
-
-
-def resource_checker(drink_choice):
-
-drink_choice = input("What would you like? (espresso/latte/cappucino): ")
-
-off = False
-
-resource_report()
-
-# TODO 1: Prompt user by asking "What would you like? (espresso/latte/cappucino)"
-# TODO 2: Turn off the Coffee Machine by entering "off" to the prompt
-# TODO 3: Print report that generate and shows current resources
-
-# TODO 4: Check that available resources are sufficient?
-# TODO 5: Process coins if resouces are sufficient
-# TODO 6: Check if transaction was successful
-# TODO 7: Make coffee
